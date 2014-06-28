3ae8e29cd620ea7043ccd8cfcd2da006732e3cd2
Ilya Radchenko <ilya@burstcreations.com>
2014-06-27 23:25:29 -0400

    get basics working

--- index.js
+++ index.js
@@ -2,31 +2,22 @@
 
 var chapterMap = require('./maps/en');
 var chapterKeys = Object.keys(chapterMap);
+var lastSection;
+var currentSection = 0;
 
-module.exports = function (passage, language) {
+module.exports = function (passages, language) {
   if (language) {
     try {
       chapterMap = require('./maps/' + language);
     } catch(e) {}
   }
 
-  var length = chapterKeys.length;
-  var index = 0;
-  var key;
-  var books = passage.match(/((\d)*(\ )*[A-Za-z])+(\ )*/g)
-    .map(trimMe);
-  var noBooks = removeBooks(passage, books);
-  var verses = noBooks.match(/(\d)+(:)*(\d)*/g).map(trimMe);
-  console.log(noBooks, verses);
-  for (; index < length; index++) {
-    key = chapterKeys[index];
-
-    if (chapterMap[key].indexOf(passage) > -1) {
-      break;
-    }
-  } 
-
-  return bookNumber(books);
+  if (typeof passages === 'number') {
+    return bookName(passages, language);
+  }
+  else if (typeof passages === 'string') {
+    return processPassages(passages, language);
+  }
 };
 
 function trimMe(item) {
@@ -42,7 +33,7 @@ function bookNumber(books) {
     for (; index < length; index++) {
       key = chapterKeys[index];
       var options = chapterMap[key];
-      if (options.indexOf(book) > -1) {
+      if (options && options.shortCodes.indexOf(book) > -1) {
         return Number(key);
       }
     }
@@ -66,4 +57,105 @@ function removeBooks(str, books) {
   result = result.replace(' ', '');
 
   return result;
+}
+
+function bookName(id, language) {
+  var valueMap = chapterMap;
+
+  if (!id) {
+    throw Error('Invalid id provided');
+  }
+
+  if (language && language !== 'en') {
+    valueMap = require('./map/' + language);
+  }
+  
+  var value = valueMap[id];
+
+  return value ? value.name : undefined;
+}
+
+function processPassages(str, languageKey) {
+  if (!str) {
+    throw new Error('Invalid passage');
+  }
+
+  return str.split(',').reduce(function (passageList, item) {
+    item = item.trim();
+
+    var result = {};
+    var sections = item.split('-');
+    
+    result = sections.reduce(function (passageObj, section) {
+      if (!passageObj.start) {
+        passageObj.start = processSection(section);
+      }
+      else {
+        passageObj.end = processSection(section);
+      }
+
+      return passageObj;
+    }, result);
+
+    passageList.push(result);
+    currentSection = 0;
+    return passageList;
+  }, []);
+}
+
+// Start/end sections
+function processSection(section) {
+  var result = {};
+  var match = section.match(/(\d*\s*[a-z]+)+/i);
+  var book = match && match.length ? match[0] : '';
+  var noBook = book ? section.replace(book, '').replace(' ', '') : section;
+  var chapterVerseSplit = noBook.split(':').filter(valid);
+
+  currentSection++;
+
+  if (book) {
+    result.book = bookKey(book);
+  }
+  
+  if (chapterVerseSplit.length) debugger;
+
+  if (chapterVerseSplit[0]) {
+    result[currentSection === 2 && isNum(lastSection.verse) && chapterVerseSplit.length === 1 ? 'verse' : 'chapter'] = Number(chapterVerseSplit[0]);
+  }
+
+  if (chapterVerseSplit[1] && !isNum(result.verse)) {
+    result.verse = Number(chapterVerseSplit[1]);
+  }
+
+  lastSection = result;
+  return result;
+}
+
+function bookKey(book,lang) {
+  return chapterKeys.map(Number).find(function (key) {
+    var chapterMeta = chapterMap[key];
+
+    return chapterMeta.shortCodes.indexOf(book) > -1;
+  });
+}
+
+Array.prototype.find = function (cb) {
+  var length = this.length;
+  var index = 0;
+  var item;
+
+  for (; index < length; index++) {
+    item = this[index];
+    if(cb(item)) {
+      return item;
+    }
+  }
+};
+
+function valid(item) {
+  return item !== undefined && item.length;
+}
+
+function isNum(prop) {
+  return prop && typeof prop === 'number';
 }
--- test/index.js
+++ test/index.js
@@ -4,19 +4,43 @@ var test = require('tape');
 var sword = require('../index');
 
 test('Books only', function (t) {
-  t.same([1], sword('gen'), 'single book');
-  t.same([1, 2], sword('gen-exo'), 'book range');
-  t.same([64], sword('1 jn'), 'book with number (space)');
-  t.same([64], sword('1jn'), 'book with number');
+  t.same(stub({ book: 1 }), sword('gen'), 'single book');
+  t.same(stub({ book: 1 }, { book: 2 }), sword('gen-exo'), 'book range');
+  t.same(stub({ book: 64 }), sword('1 jn'), 'book with number (space)');
+  t.same(stub({ book: 64 }), sword('1jn'), 'book with number');
   t.end();
 });
 
 test('Chapters', function (t) {
-  t.same([{ start: { book: 1, chapter: 1 } }], sword('gen 1'));
+  t.same(stub({ book: 1, chapter: 1 }), sword('gen 1'));
+  t.end();
+});
+
+test('Verses', function (t) {
+  t.same(stub({ book: 1, chapter: 5, verse: 4 }), sword('gen 5:4'), 'Chapter and verse');
+  t.same(stub({ book: 1, chapter: 5, verse: 4 }, { verse: 8 }), sword('gen 5:4-8'), 'Chapter and verse range (start only)');
+  t.same(stub({ book: 1, chapter: 5, verse: 4 }, { chapter: 6, verse: 8 }), sword('gen 5:4-6:8'), 'Chapter and verse range (both)');
+  t.same(stub({ book: 1, chapter: 5, verse: 4 }, { book: 2, chapter: 6, verse: 8 }), sword('gen 5:4-exo 6:8'), 'Chapter and verse range (two books)');
+  t.end();
+});
+
+test('Book Name', function (t) {
+  t.equals('Genesis', sword(1));
   t.end();
 });
 
 test('Multiple passages', function (t) {
-  t.same([1, 64], sword('gen, 1jn'), 'books only');
+  t.same(stub({ book: 1 }).concat(stub({ book: 64 })), sword('gen, 1jn'), 'books only');
+  t.same(stub({ book: 1, chapter: 15 }).concat(stub({ chapter: 16 })), sword('gen 15,16'), 'Single book, multiple chapters');
   t.end();
 });
+
+function stub(start, end) {
+  var result = [{ start: start }];
+
+  if (end) {
+    result[0].end = end;
+  }
+
+  return result;
+}
